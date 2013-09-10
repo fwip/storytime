0d361abf1f63aa5c537d42605b4d9e38104dd009
Jemma Nelson <pink.fwip@gmail.com>
2013-09-09 21:01:17 -0400

    Make editor semi-functional.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Routing
-angular.module('storytime', []).
+angular.module('storytime', ['ui.bootstrap']).
   config(['$routeProvider', function($routeProvider){
   $routeProvider.
     when('/game', {templateUrl: 'partials/game.html', controller: GameCtrl}).
-    when('/writer', {templateUrl: 'partials/writer.html', controller: GameCtrl}).
+    when('/writer', {templateUrl: 'partials/writer.html', controller: WriterCtrl}).
     when('/about', {templateUrl: 'partials/about.html'}).
     otherwise({redirectTo: '/'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,71 @@
 function GameCtrl($scope, $routeParams, $http){
-//  $scope.gameId = $routeParams.gameId;
   function load(storyname){
-    storyname = storyname || 'wizardhackers'
-    var url = "/stories/" + storyname + ".json"
+    storyname = storyname || 'wizardhackers';
+    var url = "/stories/" + storyname + ".json";
     $http({method: 'GET', url: url}).
       success(function(data, status, headers, config){
-        console.log("loaded!");
-        console.log(data);
 
-        $scope.game = data;
-        $scope.game.current_room = $scope.game.rooms["start"];
+      $scope.game = data;
+      $scope.goto(1);
 
-      }).
+    }).
       error(function(data, status, headers, config){
-        console.log("Hmm... couldn't load");
-        console.log({data: data, status: status, headers: headers, config: config});
-      });
+      console.log("Hmm... couldn't load");
+      console.log({data: data, status: status, headers: headers, config: config});
+    });
 
+    console.log("yay");
   }
 
   load('wizardhackers');
 
-  $scope.goto = function (roomname){
-    $scope.game.current_room = $scope.game.rooms[roomname];
-    console.log("yay");
+  $scope.goto = function (id){
+    $scope.game.current_place = $scope.game.places.filter(
+      function(d){ return d.id == id})[0];
+    $scope.game.available_routes = $scope.game.routes.filter(
+      function(r){ return r.from == id});
   }
+}
+
+function WriterCtrl($scope, $routeParams, $http){
+
+
+  function load(storyname){
+    storyname = storyname || 'wizardhackers'
+    var url = "/stories/" + storyname + ".json";
+    $http({method: 'GET', url: url}).
+      success(function(data, status, headers, config){
+      $scope.game = data;
+      console.log('yay');
+      console.log(JSON.stringify($scope.game));
+    }).
+      error(function(data, status, headers, config){
+      console.log("Hmm... couldn't load");
+      console.log({data: data, status: status, headers: headers, config: config});
+    });
+
+  }
+
+  $scope.alphaNum = function (string){
+    return string.replace(/[^-_A-z0-9]+/g, '');
+  }
+
+  load('wizardhackers');
+
+  $scope.addPlace = function(){
+    game.places.push( {
+      "name": "New place",
+      "routes": [],
+      "objects": [] });
+  }
+
+  $scope.addRoute = function(){
+    game.routes.push({});
+  }
+
+  $scope.addObject = function(place){
+    place.objects = place.objects || [];
+    place.objects.push({});
+  }
+
 }
